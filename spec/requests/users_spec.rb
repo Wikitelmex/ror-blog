1bb9703be5e8cc4d05f42c6725acea242b8b8413
Alex Castillo <wikitelmex@gmail.com>
2022-01-20 14:29:04 -0700

    # Linters - fix linters in testing

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
     before(:example) { get users_path }
     it 'server responses OK? (200OK)' do
       expect(response).to have_http_status(:ok)
