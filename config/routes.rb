c82efa91cc3f1e18668a9dc1473447f6c79a93ce
Alex Castillo <wikitelmex@gmail.com>
2022-01-22 10:34:33 -0700

    - add like relationships - add post relationships - add like relationships - modify user view display template - improve routing - complete schema fit to requirements - add likes table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
   root 'pages#home' # controller#method => inside the controller, we have the class and method, where we call the view
   # get 'users', to: 'users#index'
   # get 'users/:id', to: 'users#show', as: 'user' # we can use the user_path(user) to get the url
-  # get 'users/:user_id/posts', to: 'posts#index'
-  # get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post'
+  get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_171846) do
+ActiveRecord::Schema.define(version: 2022_01_22_172317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2022_01_22_171846) do
     t.integer "author_id"
     t.integer "post_id"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
