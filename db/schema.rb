79ae9b68dd18832ee28f863ee6abbbc5d2433076
Alex Castillo <wikitelmex@gmail.com>
2022-01-22 10:22:25 -0700

    Comment model
    - add migration create comment model
    - create relationship with user
    - create relationship with post

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_165029) do
+ActiveRecord::Schema.define(version: 2022_01_22_171846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "author_id"
