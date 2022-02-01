2acc23a1393f89f6231d38add142d0a2c758a5df
Ajayi Peter <peterdgreat2016@gmail.com>
2022-02-01 23:26:50 +0100

    Destroy method in like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,4 +11,13 @@ class LikesController < ApplicationController
       render plain: 'error saving like'
     end
   end
+
+  def destroy
+    @like = Like.find_by(author_id: current_user.id, post_id: params[:post_id])
+    if @like.destroy
+      redirect_to user_post_path(current_user, @like.post)
+    else
+      render plain: 'error deleting like'
+    end
+  end
 end
