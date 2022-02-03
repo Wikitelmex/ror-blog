c44ffe231e0887efce46a535ccafef3e51b1a6dc
Ajayi Peter <peterdgreat2016@gmail.com>
2022-02-03 07:40:38 -0800

    Added user_show integration test

--- spec/integration/user_show_integration_spec.rb
+++ spec/integration/user_show_integration_spec.rb
@@ -1,1 +1,48 @@
 # Peter will include tests here.
+require 'rails_helper'
+
+RSpec.describe 'User page', type: :system do
+  before do
+    Capybara.raise_server_errors = false
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post, author: @user)
+    login_as(@user, scope: :user)
+  end
+
+  after do
+    Warden.test_reset!
+  end
+
+  describe 'show page' do
+    it 'shows the right profile picture' do
+      visit user_path(@user)
+      expect(page.html).to match(@user.photo)
+    end
+
+    it 'shows the right username' do
+      visit user_path(@user)
+      expect(page).to have_content(@user.name)
+    end
+
+    it 'shows the right posts counter' do
+      visit user_path(@user)
+      expect(page).to have_content("Number of posts: #{@user.posts.count}")
+    end
+
+    it 'shows the right user bio' do
+      visit user_path(@user)
+      expect(page).to have_content(@user.bio)
+    end
+
+    it 'shows the right user firs posts' do
+      visit user_path(@user)
+      expect(page).to have_content(@user.recent_posts[0].title)
+    end
+
+    # it 'shows the right username' do
+  
+    #   visit user_path(@user)
+    #   expect(page).to have_content("photo.jpg included")
+    # end
+  end
+end
