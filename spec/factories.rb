80d5721f8a00dee80f5bb75c15719dce5035bb1f
Alex Castillo <wikitelmex@gmail.com>
2022-02-03 08:28:59 -0700

    - add Post factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,11 +9,25 @@ FactoryBot.define do
     updated_at { DateTime.now }
     name { 'alex user name' }
     role { 'admin' }
-    photo { 'photo.jpg included' }
+    photo { 'include_photo.jpg' }
     postscounter { 1234 }
-    # using dynamic attributes over static attributes in FactoryBot
+    bio { 'long bio description' }
+  end
 
-    # if needed
-    # is_active true
+  factory :post do
+    id { 991 }
+    author_id { 999 }
+    title { 'post title' }
+    text { 'post text 1234' }
+    comments_counter { 1234 }
+    likes_counter { 1234 }
+    created_at { DateTime.now }
+    updated_at { DateTime.now }
   end
 end
+
+# recent_posts { [
+#   {author_id: 999, id: 991, title: 'title1', text: 'text1', created_at: DateTime.now, updated_at: DateTime.now},
+#   {author_id: 999, id: 992, title: 'title2', text: 'text2', created_at: DateTime.now, updated_at: DateTime.now},
+#   {author_id: 999, id: 993, title: 'title3', text: 'text3', created_at: DateTime.now, updated_at: DateTime.now}
+# ] }
---
+++ spec/integration/user_show_integration_spec.rb
@@ -0,0 +1,1 @@
+# Peter will include tests here.
